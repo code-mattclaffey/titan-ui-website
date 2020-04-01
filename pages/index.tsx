d33d43178b0e29d659c2e345eba7f880331382a0
code-mattclaffey <mattclaffey93@gmail.com>
2020-04-01 15:45:39 +0100

    feat: :tada: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Heading } from '@titan-tooling/ui';
+import '../styles/main.css';
+
+const Home = () => <Heading as="h1">Hello World</Heading>;
+
+export default Home;
